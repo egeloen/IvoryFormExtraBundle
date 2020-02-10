53f89eafcf5d90c9cd9166641d2ef65a77cc3933
Nogrod <strikeomania@gmail.com>
2020-02-10 15:12:36 +0100

    fixes

--- Twig/FormExtraExtension.php
+++ Twig/FormExtraExtension.php
@@ -12,11 +12,13 @@
 namespace Ivory\FormExtraBundle\Twig;
 
 use Symfony\Bridge\Twig\Node\SearchAndRenderBlockNode;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
 /**
  * @author GeLo <geloen.eric@gmail.com>
  */
-class FormExtraExtension extends \Twig\Extension\AbstractExtension
+class FormExtraExtension extends AbstractExtension
 {
     /**
      * {@inheritdoc}
@@ -29,8 +31,8 @@ class FormExtraExtension extends \Twig\Extension\AbstractExtension
         ];
 
         return [
-            new \Twig_SimpleFunction('form_javascript', null, $options),
-            new \Twig_SimpleFunction('form_stylesheet', null, $options),
+            new TwigFunction('form_javascript', null, $options),
+            new TwigFunction('form_stylesheet', null, $options),
         ];
     }
 
